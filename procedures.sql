93003aee43e96b97436b7ef81806b17c8b83415e
Steven Howard <stevenhoward@u.northwestern.edu>
2011-10-17 03:03:17 +0000

    Added stored procedures

---
+++ procedures.sql
@@ -0,0 +1,7 @@
+drop procedure if exists usp_insert_note;
+delimiter //
+create procedure usp_insert_note(deviceId CHAR(36), author VARCHAR(32), location VARCHAR(255), notes VARCHAR(255))
+begin
+    insert into notes(device_id, user_name, time, location_description, note_text)
+                values(deviceId, author, NOW(), location, notes);
+end//
