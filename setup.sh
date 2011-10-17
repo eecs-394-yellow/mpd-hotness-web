de1c3e284d160a406535b1e48ba1c8ebde99456a
Steven Howard <stevenhoward@u.northwestern.edu>
2011-10-17 21:12:52 +0000

    Created initialization script which will set configuration options and make appropriate links from the repo directory to the web directory.

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+getrelpath() {
+    python -c "import os.path; print(os.path.relpath('$1', '$2'))"
+}
+
+if [[ -z $1 ]] ; then
+    echo "usage: $0 dest-dir"
+    exit 1
+fi
+
+DESTDIR=$1
+PREFIX=$(getrelpath . $DESTDIR)
+
+for phpfile in index.php list.php ; do
+    ln -sf "${PREFIX}/${phpfile}" "$DESTDIR"
+done
+
+cp config.php $DESTDIR
+sed -i '' "s@PROTECTED_DIR_PATH@'${PREFIX}'@" "${DESTDIR}/config.php"
