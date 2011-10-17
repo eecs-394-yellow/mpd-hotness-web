ac0a23eccd61739bd0db87246293115f75b9188f
Steven Howard <stevenhoward@u.northwestern.edu>
2011-10-16 22:05:53 -0500

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -3,5 +3,5 @@ delimiter //
 create procedure usp_insert_note(deviceId CHAR(36), author VARCHAR(32), location VARCHAR(255), notes VARCHAR(255))
 begin
     insert into Note(device_id, user_name, time, location_description, note_text)
-                values(deviceId, author, NOW(), location, notes);
+                values(uuid_to_binary(deviceId), author, NOW(), location, notes);
 end//
