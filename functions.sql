fffd6eb98a26b7d73bbb1552f658169a64aadb23
Steven Howard <stevenhoward@u.northwestern.edu>
2011-10-17 03:41:23 +0000

    For some damn reason MySQL substr starts from index 1 because the designers don't like doing what the rest of the damn world does when it comes to commonly-named string functions.

--- functions.sql
+++ functions.sql
@@ -1,19 +1,19 @@
-drop function binary_from_uuid;
+drop function if exists binary_from_uuid;
 
 create function binary_from_uuid(uuid CHAR(36))
   returns binary(16)
   return unhex(replace(uuid, '-', ''));
 
-drop function uuid_from_binary;
+drop function if exists uuid_from_binary;
 delimiter //
 create function uuid_from_binary(uuid_bin binary(16))
 returns char(36)
 begin
     set @uuid = hex(uuid_bin);
-    return concat_ws('-', substring(@uuid from 0 for 8), 
-                         substring(@uuid from 8 for 4), 
-                         substring(@uuid from 12 for 4), 
-                         substring(@uuid from 16 for 4),
-                         substring(@uuid from 20 for 12));
+    return concat_ws('-', substring(@uuid from 1 for 8), 
+                          substring(@uuid from 9 for 4), 
+                          substring(@uuid from 13 for 4), 
+                          substring(@uuid from 17 for 4),
+                          substring(@uuid from 21 for 12));
 end//
 delimiter ;
