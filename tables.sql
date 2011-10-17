ebe4573d63320f1cdde2adbfaf0bbbc2f3b73165
Steven Howard <stevenhoward@u.northwestern.edu>
2011-10-17 01:43:17 +0000

    Added initial scripts to set up tables and apply changes

---
+++ tables.sql
@@ -0,0 +1,15 @@
+drop table if exists Note;
+
+create table Note (
+    -- device UUID
+    device_id varbinary(16) not null,
+    -- username (not really verified)
+    user_name varchar(32) not null,
+    -- posting time
+    time datetime not null,
+    -- user's description of their location
+    location_description varchar(255) not null,
+    -- text of user's note
+    note_text varchar(255) not null,
+    primary key(device_id, time)
+);
